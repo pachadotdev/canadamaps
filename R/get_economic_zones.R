59116f063d56f9eec12baa7f8a4848860ec5a0a2
Mauricio Vargas <mvargas@dcc.uchile.cl>
2021-05-09 03:24:09 -0400

    faster economic region

--- R/get_economic_zones.R
+++ R/get_economic_zones.R
@@ -2,7 +2,7 @@
 #' @description Este mapa es calculado a partir
 #' del mapa censal para no recargar el volumen de datos del paquete.
 #' @param mapa mapa a agregar, por defecto es todo el mapa nacional
-#' @importFrom rmapshaper ms_dissolve
+#' @importFrom rmapshaper ms_dissolve ms_simplify
 #' @importFrom sf st_as_sf
 #' @importFrom dplyr as_tibble filter select left_join
 #' @importFrom magrittr %>%
@@ -15,21 +15,17 @@ get_economic_zones <- function(map = census_divisions) {
     filter(cduid != 3524) %>%
     left_join(canadamaps::cduid_eruid, by = "cduid")
 
+  map <- map %>%
+    bind_rows(
+      halton_special_case %>% select(-cduid, -cdname, -cdtype)
+    )
+
   eruid_pruid <- map %>%
     as_tibble() %>%
     select(eruid, ername, pruid, prname) %>%
     distinct()
 
-  map <- ms_dissolve(st_as_sf(map), field = "eruid")
-
-  map <- map %>%
-    bind_rows(
-      halton_special_case %>% select(eruid, geometry)
-    )
-
-  map <- ms_dissolve(st_as_sf(map), field = "eruid")
-
-  map <- map %>%
+  map <- ms_dissolve(st_as_sf(map), field = "eruid") %>%
     as_tibble() %>%
     left_join(eruid_pruid, by = "eruid")
 
